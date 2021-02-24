4421ae41711b7f4bea10436b36fc955f74e504f4
pedropbazzo <pedropbazzo@outlook.com>
2021-02-24 13:36:58 -0300

    feat: update service

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,  HttpHeaders  } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 interface Search {};
@@ -7,23 +7,12 @@ interface Search {};
 @Injectable()
 export class SearchService
 {
-   /**
-    * Url service Api Github
-    */
     public api = 'https://api.github.com/users';
-
-    /**
-     * 
-     * @param http 
-     */
+      
     constructor( private http: HttpClient ) {
         console.log('Api github services');
     }
 
-    /**
-     * 
-     * @param search 
-     */
     getSearch(search): Observable<Search[]>{
         return this.http.get<Search[]>(`${this.api}/${search}/repos`);
     }
